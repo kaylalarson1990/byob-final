a4f4b4570e0ee386fdcc56b89d023833ba035922
Kayla <kayla.larson1990@gmail.com>
2019-08-17 11:52:04 -0600

    Fix knex file with seed data for production

--- knexfile.js
+++ knexfile.js
@@ -6,13 +6,20 @@ module.exports = {
       directory: "./migrations"
     },
     seeds: {
-      directory: "./seeds/dev/shows.js"
+      directory: "./seeds/dev"
     },
     useNullAsDefault: true
   },
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: "./migrations"
+    },
+    useNullAsDefault: true
+  },
+  seeds: {
+    directory: "./seeds/dev"
   }
 };
